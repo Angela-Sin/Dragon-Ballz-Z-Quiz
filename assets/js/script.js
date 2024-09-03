d70ec14c158486daa817f93a44c81fdee11114c3
Anzela Siniavskaja <v6zmaqkl@students.codeinstitute.net>
2024-09-03 14:46:19 +0000

    add querry selector in java for quiz container

---
+++ assets/js/script.js
@@ -0,0 +1,4 @@
+const container = document.querySelector(".container");
+const questionBox = document.querySelector(".question");
+const choicesBox = document.querySelector(".choices");
+const nextBtn = document.querySelector(".nextBtn");
