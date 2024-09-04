8c1d8293c6f40b54f923c399947426cbe0da7f53
Anzela Siniavskaja <v6zmaqkl@students.codeinstitute.net>
2024-09-04 11:19:44 +0000

    fixed bug with questions, owewrited the same command twice. Thanks for tutor support

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,44 +22,43 @@ const quiz = [
     choices: ["Namek", "Sayan", "Truffle"],
     answer: "Namek",
   },
+  {
+    question: "Q.: What do all the dragon balls have inside?",
+    choices: ["Crystals", "Stars", "Dragons"],
+    answer: "Stars",
+  },
   //{
-    //question: "Q.: What do all the dragon balls have inside?",
-   // choices: ["Crystals", "Stars", "Dragons"],
-   // answer: "Stars",
+  //question: "Q.: Krillin trained under",
+  //choices: ["Picolo", "Tien", "Master Roshi"],
+  //answer: "Master Roshi",
   //},
   //{
-    //question: "Q.: Krillin trained under",
-    //choices: ["Picolo", "Tien", "Master Roshi"],
-    //answer: "Master Roshi",
+  // question:
+  //  "Q.: This wise-cracking little pig-creature can shapeshift (well, for a few minutes):",
+  //choices: ["Oolong", "Ox King", "King Yemma"],
+  //answer: "Oolong",
+  // },
+  // {
+  // question: "Q.: Garlic Jr.'s power is greatest when he's closest to",
+  // choices: ["Kami", "Shenron", "The Makyo Star"],
+  // answer: "The Makyo Star",
   //},
   //{
-   // question:
-    //  "Q.: This wise-cracking little pig-creature can shapeshift (well, for a few minutes):",
-    //choices: ["Oolong", "Ox King", "King Yemma"],
-    //answer: "Oolong",
- // },
- // {
-   // question: "Q.: Garlic Jr.'s power is greatest when he's closest to",
-   // choices: ["Kami", "Shenron", "The Makyo Star"],
-   // answer: "The Makyo Star",
+  // question: "Q.: Dr. Gero works for",
+  // choices: ["The Red Ribbon Army", "The Spice Boys", "Androids 19 and 20"],
+  // answer: "The Red Ribbon Army",
   //},
   //{
-   // question: "Q.: Dr. Gero works for",
-   // choices: ["The Red Ribbon Army", "The Spice Boys", "Androids 19 and 20"],
-   // answer: "The Red Ribbon Army",
-  //},
-  //{
-   // question: "Q.: When Goku's energy level goes way up, his eyes turn",
-   // choices: ["Red", "Green", "Gold"],
+  // question: "Q.: When Goku's energy level goes way up, his eyes turn",
+  // choices: ["Red", "Green", "Gold"],
   //  answer: "Green",
   //},
- // {
+  // {
   //  question: "Q.: Like his father, Gohan had this when he was little:",
   //  choices: ["A cat", "A Flying disc", "A tail"],
   //  answer: "A tail",
   //},
 ];
-
 
 // Making Variables
 let currentQuestionIndex = 0;
@@ -69,25 +68,26 @@ let quizOver = false;
 // Arrow Function to Show Questions
 const showQuestions = () => {
   const questionDetails = quiz[currentQuestionIndex];
+
   questionBox.textContent = questionDetails.question;
 
-   choicesBox.textContent = "";
-   for (let i = 0; i < questionDetails.choices.length; i++) {
+  choicesBox.textContent = "";
+  for (let i = 0; i < questionDetails.choices.length; i++) {
     const currentChoice = questionDetails.choices[i];
     const choiceDiv = document.createElement("div");
     choiceDiv.textContent = currentChoice;
     choiceDiv.classList.add("choice");
     choicesBox.appendChild(choiceDiv);
 
-      choiceDiv.addEventListener("click", () => {
+    choiceDiv.addEventListener("click", () => {
       if (choiceDiv.classList.contains("selected")) {
         choiceDiv.classList.remove("selected");
       } else {
         choiceDiv.classList.add("selected");
-    }
-});
-}
-}   
+      }
+    });
+  }
+};
 
 // Function to check answers
 const checkAnswer = () => {
@@ -96,68 +96,63 @@ const checkAnswer = () => {
     //alert("Correct Answer!");
     displayAlert("Correct Answer!");
     score++;
-    } else {
-        //alert("Wrong Answer!");
-        displayAlert("Wrong Answer!");
-    }
-    currentQuestionIndex++;
-    if (currentQuestionIndex < quiz.length) {
-      currentQuestionIndex++;
-      showQuestions();
-    } else {
-      showScore();
-      quizOver = true;
-    }
-//console.log(selectedChoice);
-}
-
-
+  } else {
+    //alert("Wrong Answer!");
+    displayAlert("Wrong Answer!");
+  }
+  currentQuestionIndex++;
+  if (currentQuestionIndex < quiz.length) {
+    showQuestions();
+  } else {
+    showScore();
+    quizOver = true;
+  }
+  //console.log(selectedChoice);
+};
 
 //Show score function
 
 const showScore = () => {
   questionBox.textContent = "";
   choicesBox.textContent = "";
-scoreCont.textContent = `You Scored ${score} out of ${quiz.length}!`;
- displayAlert("You have completed this quiz!");
-nextBtn.textContent = "Play Again";
-//nextBtn.addEventListener('click', ()=>{
-//currentQuestionIndex = 0;
-//showQuestions();
-//nextBtn.textContent = "Next";
-//scoreCont.textContent = "";
-//});
-
-} 
+  scoreCont.textContent = `You Scored ${score} out of ${quiz.length}!`;
+  displayAlert("You have completed this quiz!");
+  nextBtn.textContent = "Play Again";
+  //nextBtn.addEventListener('click', ()=>{
+  //currentQuestionIndex = 0;
+  //showQuestions();
+  //nextBtn.textContent = "Next";
+  //scoreCont.textContent = "";
+  //});
+};
 
 // Function to Show Alert
 const displayAlert = (msg) => {
   alert.style.display = "block";
   alert.textContent = msg;
-   setTimeout(() =>{
-  alert.style.display = "none";
+  setTimeout(() => {
+    alert.style.display = "none";
   }, 1000);
-}
+};
 
 showQuestions();
-nextBtn.addEventListener('click', () =>{
-  const selectedChoice = document.querySelector('.choice.selected');
-  if(!selectedChoice && nextBtn.textContent === "Next"){
+nextBtn.addEventListener("click", () => {
+  const selectedChoice = document.querySelector(".choice.selected");
+  if (!selectedChoice && nextBtn.textContent === "Next") {
     //alert("Select Answer");
     displayAlert("Select Answer");
     return;
-  } 
-  if(quizOver){
-    //currentQuestionIndex = 0; 
+  }
+  if (quizOver) {
+    //currentQuestionIndex = 0;
     nextBtn.textContent = "Next";
     scoreCont.textContent = "";
     currentQuestionIndex = 0;
-    showQuestions();
+
     quizOver = false;
     score = 0;
-  }
-  else{
+    showQuestions();
+  } else {
     checkAnswer();
-  } 
-    
+  }
 });
